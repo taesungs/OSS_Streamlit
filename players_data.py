c5e6db73f16c76c22c9af0fc8733f88c60082688
taesungs <70329288+taesungs@users.noreply.github.com>
2026-04-26 15:47:11 +0900

    feat: mlb 퀴즈 생성

---
+++ players_data.py
@@ -0,0 +1,67 @@
+def get_players():
+    return [
+        {
+            "name": "오타니 쇼헤이",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_426,q_auto:best/v1/people/660271/headshot/67/current"
+        },
+        {
+            "name": "애런 저지",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/592450/headshot/67/current"
+        },
+        {
+            "name": "무키 베츠",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/605141/headshot/67/current"
+        },
+        {
+            "name": "마이크 트라웃",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/545361/headshot/67/current"
+        },
+        {
+            "name": "브라이스 하퍼",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/547180/headshot/67/current"
+        },
+        {
+            "name": "후안 소토",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/665742/headshot/67/current"
+        },
+        {
+            "name": "페르난도 타티스 주니어",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/665487/headshot/67/current"
+        },
+        {
+            "name": "이정후",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/808982/headshot/67/current"
+        },
+        {
+            "name": "프레디 프리먼",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/518692/headshot/67/current"
+        },
+        {
+            "name": "김혜성",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/808975/headshot/67/current"
+        },
+        {
+            "name": "야마모토 요시노부",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/808967/headshot/67/current"
+        },
+        {
+            "name": "폴 스킨스",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/694973/headshot/67/current"
+        },
+        {
+            "name": "타릭 스쿠발",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/669373/headshot/67/current"
+        },
+        {
+            "name": "메이슨 밀러",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_426,q_auto:best/v1/people/695243/headshot/67/current"
+        },
+        {
+            "name": "에드윈 디아스",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/621242/headshot/67/current"
+        },
+        {
+            "name": "매니 마차도",
+            "image": "https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/592518/headshot/67/current"
+        }
+    ]
